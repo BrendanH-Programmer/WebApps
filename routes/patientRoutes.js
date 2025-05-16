8d6e1a2c45af0ab3069461e50f76286c4eb17a1e
Brendan Henderson <brendanhenderson@live.co.uk>
2025-05-16 11:01:49 +0000

    paitent and room routes updated

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const patientController = require("../controllers/patientController");
+const { isAuthenticated } = require("../middleware/authMiddleware");
 
 router.get("/", patientController.index);
 router.get("/new", patientController.new);
@@ -8,5 +9,6 @@ router.post("/", patientController.create);
 router.get("/:id/edit", patientController.edit);
 router.put("/:id", patientController.update);
 router.delete("/:id", patientController.remove);
+router.get("/", isAuthenticated, patientController.index);
 
 module.exports = router;
--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const roomController = require("../controllers/roomController");
+const { isAuthenticated } = require("../middleware/authMiddleware");
 
 router.get("/", roomController.index);
+router.get("/", isAuthenticated, patientController.index);
 
 module.exports = router;

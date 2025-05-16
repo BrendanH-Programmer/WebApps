6f943a633aae244cbbf6317108de877b936ce316
Brendan Henderson <brendanhenderson@live.co.uk>
2025-05-16 16:36:35 +0000

    pateint controller updated

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,4 +1,6 @@
 const Patient = require("../models/Patient");
+const { assignRoomToPatient } = require("../utils/roomAssigner");
+
 exports.index = async (req, res) => {
   const patients = await Patient.find().populate("roomAssigned");
   res.render("patients/index", { patients });
@@ -9,9 +11,25 @@ exports.new = (req, res) => {
 };
 
 exports.create = async (req, res) => {
-  const patient = new Patient(req.body);
-  await patient.save();
-  res.redirect("/patients");
+  try {
+    const patient = new Patient(req.body);
+    const assignedRoom = await assignRoomToPatient(patient);
+
+    if (assignedRoom) {
+      patient.roomAssigned = assignedRoom._id;
+      await patient.save();
+
+      assignedRoom.currentPatients.push(patient._id);
+      await assignedRoom.save();
+
+      res.redirect("/patients");
+    } else {
+      res.send("No suitable room available");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error creating patient");
+  }
 };
 
 exports.edit = async (req, res) => {
@@ -27,23 +45,4 @@ exports.update = async (req, res) => {
 exports.remove = async (req, res) => {
   await Patient.findByIdAndDelete(req.params.id);
   res.redirect("/patients");
-};
-
-const { assignRoomToPatient } = require("../utils/roomAssigner");
-
-exports.create = async (req, res) => {
-  const patient = new Patient(req.body);
-  const assignedRoom = await assignRoomToPatient(patient);
-
-  if (assignedRoom) {
-    patient.roomAssigned = assignedRoom._id;
-    await patient.save();
-
-    assignedRoom.currentPatients.push(patient._id);
-    await assignedRoom.save();
-
-    res.redirect("/patients");
-  } else {
-    res.send("No suitable room available");
-  }
 };

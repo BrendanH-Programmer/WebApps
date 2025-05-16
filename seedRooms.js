bd11396b686b1caaf5dacf43a9289ecb6b23620b
Brendan Henderson <brendanhenderson@live.co.uk>
2025-05-16 17:03:06 +0000

    isolation room indicater added

--- seedRooms.js
+++ seedRooms.js
@@ -10,17 +10,17 @@ async function seedRooms() {
     console.log("🧹 Existing rooms cleared");
 
     const rooms = [
-      { name: "Room 101", capacity: 2, currentPatients: [], isIsolation: false },
-      { name: "Room 102", capacity: 1, currentPatients: [], isIsolation: true },
-      { name: "Room 103", capacity: 3, currentPatients: [], isIsolation: false },
-      { name: "Room 104", capacity: 2, currentPatients: [], isIsolation: false },
-      { name: "Room 105", capacity: 1, currentPatients: [], isIsolation: true },
-      { name: "Room 106", capacity: 4, currentPatients: [], isIsolation: false },
-      { name: "Room 107", capacity: 2, currentPatients: [], isIsolation: false },
-      { name: "Room 108", capacity: 1, currentPatients: [], isIsolation: true },
-      { name: "Room 109", capacity: 3, currentPatients: [], isIsolation: false },
-      { name: "Room 110", capacity: 2, currentPatients: [], isIsolation: false },
-      { name: "Room 111", capacity: 1, currentPatients: [], isIsolation: true }
+      { name: "101", capacity: 2, currentPatients: [], isIsolation: false },
+      { name: "102", capacity: 1, currentPatients: [], isIsolation: true },
+      { name: "103", capacity: 3, currentPatients: [], isIsolation: false },
+      { name: "104", capacity: 2, currentPatients: [], isIsolation: false },
+      { name: "105", capacity: 1, currentPatients: [], isIsolation: true },
+      { name: "106", capacity: 4, currentPatients: [], isIsolation: false },
+      { name: "107", capacity: 2, currentPatients: [], isIsolation: false },
+      { name: "108", capacity: 1, currentPatients: [], isIsolation: true },
+      { name: "109", capacity: 3, currentPatients: [], isIsolation: false },
+      { name: "110", capacity: 2, currentPatients: [], isIsolation: false },
+      { name: "111", capacity: 1, currentPatients: [], isIsolation: true }
     ];
 
     await Room.insertMany(rooms);

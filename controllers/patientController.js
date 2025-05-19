ba7f42cde57b25719d23aef2a835f92c26333b5f
Brendan Henderson <brendanhenderson@live.co.uk>
2025-05-19 11:59:11 +0000

    symotoms updated

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -12,9 +12,16 @@ exports.new = (req, res) => {
 
 exports.create = async (req, res) => {
   try {
-    const patient = new Patient(req.body);
-    const assignedRoom = await assignRoomToPatient(patient);
+    const patient = new Patient({
+      title: req.body.title,
+      firstName: req.body.firstName,
+      surname: req.body.surname,
+      age: req.body.age,
+      symptoms: req.body.symptoms.split(',').map(s => s.trim()),
+      infectionRisk: req.body.infectionRisk,
+    });
 
+    const assignedRoom = await assignRoomToPatient(patient);
     if (assignedRoom) {
       patient.roomAssigned = assignedRoom._id;
       await patient.save();
@@ -31,6 +38,7 @@ exports.create = async (req, res) => {
     res.status(500).send("Error creating patient");
   }
 };
+
 
 exports.edit = async (req, res) => {
   const patient = await Patient.findById(req.params.id);
---
+++ utils/symptomRisk.js
@@ -0,0 +1,10 @@
+module.exports = {
+  cough: 2,
+  cold: 2,
+  fever: 3,
+  fatigue: 1,
+  headache: 1,
+  soreThroat: 3,
+  shortnessOfBreath: 2,
+  lossOfSmell: 3
+};

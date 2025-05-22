ea671cc95b320fd0795480ef5ab4a6ae075eb6a1
Brendan Henderson <brendanhenderson@live.co.uk>
2025-05-22 09:51:00 +0000

    homepage added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 const roomController = require("../controllers/roomController");
-const patientController = require("../controllers/patientController"); 
+const patientController = require("../controllers/patientController");
 const { isAuthenticated, isAdmin, allowRoles } = require("../utils/authMiddleware");
 
 // Auth routes
@@ -11,9 +11,18 @@ router.post("/register", authController.register);
 router.get("/login", (req, res) => res.render("login"));
 router.post("/login", authController.login);
 
+// Homepage/dashboard (protected)
+router.get("/homepage", isAuthenticated, (req, res) => {
+  res.render("homepage", { user: req.session.user });
+});
 
-// Protected routes
-router.post("/rooms", isAuthenticated, isAdmin, roomController.createRoom);
+// Patients routes
 router.get("/patients", isAuthenticated, allowRoles(["admin", "nurse"]), patientController.index);
+router.get("/patients/new", isAuthenticated, allowRoles(["admin", "nurse"]), patientController.new);
+router.post("/patients", isAuthenticated, allowRoles(["admin", "nurse"]), patientController.create);
+
+// Rooms routes
+router.get("/rooms", isAuthenticated, allowRoles(["admin", "nurse"]), roomController.index);
+router.post("/rooms", isAuthenticated, isAdmin, roomController.createRoom);
 
 module.exports = router;
